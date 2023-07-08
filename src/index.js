5c3b15de720a1335a7844fdaa04a36281bbf490d
amirnasr <amir.nasr72@gmail.com>
2023-07-08 10:43:03 +0330

    config routProvider in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import {RouterProvider} from "react-router-dom";
+import {router} from "./routes"
 import App from './App';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </StrictMode>
 );
